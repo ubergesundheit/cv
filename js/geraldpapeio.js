db49f3865e9dff5863aa530327d54d4b29730f13
ubergesundheit <ubergesundheit@googlemail.com>
2019-03-19 22:40:19 +0100

    defer js and modernize loop

--- js/geraldpapeio.js
+++ js/geraldpapeio.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var links = document.querySelectorAll(".obfusmail");
-  for (var i = 0; i < links.length; i++) {
-    var link = links[i];
-    var address = link.dataset.href;
-    link.href = address.split('').reverse().join('');
-  }
-})
+for (var link of document.querySelectorAll('.obfusmail')) {
+  links.href = link.dataset.href.split('').reverse().join('');
+}
