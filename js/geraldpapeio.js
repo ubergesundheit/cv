f28403600db68068f4f7ba64c5a374de2e302fad
ubergesundheit <ubergesundheit@googlemail.com>
2019-06-21 18:19:48 +0200

    try to fix js

--- js/geraldpapeio.js
+++ js/geraldpapeio.js
@@ -1,3 +1,9 @@
-for (var link of document.querySelectorAll('.obfusmail')) {
-  links.href = link.dataset.href.split('').reverse().join('');
+"use strict";
+
+function reverseLinks() {
+  for (var link of document.querySelectorAll('.obfusmail')) {
+    links.href = link.dataset.href.split('').reverse().join('');
+  }
 }
+
+window.addEventListener('DOMContentLoaded', reverseLinks);
